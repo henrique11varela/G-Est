501b23515a0f377779a002c777e2a963896d4118
Henrique Varela <henrique11varela@gmail.com>
2023-12-30 22:09:41 +0000

    it works

---
+++ README.md
@@ -0,0 +1,30 @@
+# G-Est
+
+This is a Repo that puts G-Est_frontend and G-Est_backend together
+
+## Usage
+
+``` shell
+# Clone repos using "dev" branch and installs dependencies
+./commands.sh run setup
+# Start
+./commands.sh start
+# Migrate backend
+./commands.sh run migrate
+# Stop
+./commands.sh stop
+```
+
+## Start
+
+``` shell
+./commands.sh run setup && \
+./commands.sh start && \
+./commands.sh run migrate
+```
+
+## Stop
+
+``` shell
+./commands.sh stop
+```
--- commands.sh
+++ commands.sh
@@ -27,7 +27,18 @@ function confirm(){
 # ./commands.sh start
 function startProject(){
 	failed=1
-	docker-compose up -d && echo "docker app started" && echo "Ctrl + Click: http://localhost:9000" && failed=0
+	# docker-compose up -d && echo "docker app started" && echo "Ctrl + Click: http://localhost:9000" && failed=0
+	cd G-Est_backend && ./commands.sh start && \
+	cd .. && \
+	cd G-Est_frontend && ./commands.sh start && \
+	cd .. && \
+	echo "" && \
+	echo "########################################" && \
+	echo "# ~> phpmyadmin: http://localhost:8081 #" && \
+	echo "# ~> laravel:    http://localhost:8080 #" && \
+	echo "# ~> quasar:     http://localhost:80   #" && \
+	echo "########################################" && \
+	failed=0
 	if [ $failed -eq 1 ]
 	then
 		echo "docker app failed to start"
@@ -37,67 +48,43 @@ function startProject(){
 # ./commands.sh stop
 function stopProject(){
 	failed=1
-	docker-compose down && echo docker app stopped && failed=0
+	cd G-Est_frontend && ./commands.sh stop && \
+	cd .. && \
+	cd G-Est_backend && ./commands.sh stop && \
+	cd .. && \
+	failed=0
 	if [ $failed -eq 1 ]
 	then
 		echo "docker app failed to stop"
 	fi
 }
 
-function envDevFill(){
-    # .env Constants
-    env_path="G-Est_backend/src/.env"
-    env_example_path="G-Est_backend/src/.env.example"
-    APP_KEY="base64:nEBQ1DB+qSBkkWYasazlciiHNDRNt3GGHE1AHm3I1nc=" # static key for development
-    DB_HOST="mysql" # database docker container name
-    DB_DATABASE="homestead" # database name
-    DB_USERNAME="homestead" # connection user name
-    DB_PASSWORD="secret" # connection user password
-
-    rm -f $env_path
-    while IFS="=" read -r envKey envValue
-    do
-        case $envKey in
-            APP_KEY)
-                echo "$envKey=$APP_KEY" >> $env_path
-                ;;
-            DB_HOST)
-                echo "$envKey=$DB_HOST" >> $env_path
-                ;;
-            DB_DATABASE)
-                echo "$envKey=$DB_DATABASE" >> $env_path
-                ;;
-            DB_USERNAME)
-                echo "$envKey=$DB_USERNAME" >> $env_path
-                ;;
-            DB_PASSWORD)
-                echo "$envKey=$DB_PASSWORD" >> $env_path
-                ;;
-            *)
-                if [ -n "$envKey" ]
-                then
-                    echo "$envKey=$envValue" >> $env_path
-                else
-                    echo "" >> $env_path
-                fi
-                ;;
-        esac
-    done < $env_example_path
-	echo ".env file updated for development"
-}
-
 # ./commands.sh run setup
 function setupProject(){
-	./G-Est_backend/commands.sh run setup && \
-	./G-Est_frontend/commands.sh run setup
+	(git clone -b dev https://github.com/henrique11varela/G-Est_backend.git || true) && \
+	(git clone -b dev https://github.com/henrique11varela/G-Est_frontend.git || true) && \
+	cd G-Est_backend && ./commands.sh run setup && \
+	cd .. && \
+	cd G-Est_frontend && ./commands.sh run setup && \
+	cd .. && \
 	echo "docker app setup"
+}
+
+function migrateProject(){
+	failed=1
+	cd G-Est_backend && ./commands.sh run migrate && \
+	cd .. && \
+	failed=0
+	if [ $failed -eq 1 ]
+	then
+		echo "docker app failed to migrate"
+	fi
 }
 
 # Main
 case $1 in
 	# start
 	start)
-		envDevFill
 		startProject
 		;;
 	# stop
@@ -109,8 +96,11 @@ case $1 in
 		case $2 in
 			# run setup
 			setup)
-				envDevFill
 				setupProject
+				;;
+			# run migrate
+			migrate)
+				migrateProject
 				;;
 			# run ____
 			*)
